fab2f3250241c327b0a2cdaa397b8ae08bbadfa7
taidertot25 <tylerhimes25@gmail.com>
2024-07-19 07:40:11 -0400

    working on zoom camp from start

--- 01-docker-terraform/2_docker_sql/pipeline.py
+++ 01-docker-terraform/2_docker_sql/pipeline.py
@@ -1,11 +1,5 @@
-import sys
-
-import pandas as pd
-
-print(sys.argv)
-
-day = sys.argv[1]
+import pandas as pd 
 
 # some fancy stuff with pandas
 
-print(f'job finished successfully for day = {day}')
+print('job finished successfully')
